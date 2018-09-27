a06ccd41ef16c264f3f911152060fb8c8a8b13cb
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2018-09-27 11:34:03 -0300

    Make dummy tests

--- tests/main/test_views.py
+++ tests/main/test_views.py
@@ -2,21 +2,21 @@ class Test_AddProductToCatalog():
     def test_add_product_to_catalog(self, client):
         pass
 
-    def test_add_existing_product_to_catalog():
+    def test_add_existing_product_to_catalog(self, client):
         pass
 
 
 class Test_AddSpecificationToProduct():
-    def test_add_specification_to_product():
+    def test_add_specification_to_product(self, client):
         pass
 
-    def test_add_existing_specification_to_product():
+    def test_add_existing_specification_to_product(self, client):
         pass
 
 
 class Test_DetailProduct():
-    def test_404():
+    def test_404(self, client):
         pass
 
-    def test_detail_product():
+    def test_detail_product(self, client):
         pass
